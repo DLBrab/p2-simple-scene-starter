4470545b0101dc63164833c20fbe740bfe0513bb
DLBrab <82184836+DLBrab@users.noreply.github.com>
2022-06-25 23:34:40 -0700

    Change to modify ellipse shapes

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function draw() {
   
   distanceBetweenSpotsX = 20 + random() * 20;
   distanceBetweenSpotsY = 10 + random() * 20;
+  
 
 
   offsetX = (mouseX - (width / 3)) / 10 // Creates mouse responsiveness in generation
@@ -28,26 +29,29 @@ function draw() {
   if (offsetY > 0) offsetY = 0
 
   for (i = 0; i < height; i = i + distanceBetweenSpotsY) {
+    
     for (j = width; j > 0; j = j - distanceBetweenSpotsX) {
       distanceBetweenSpotsX = 20 + random() * 60
       distanceBetweenSpotsY = random() * 10 - 10
+      radYTweak = 0.5 + random();
       fill (iceLightColor)
-      ellipse(j - offsetX / 3,i - offsetY / 3 + distanceBetweenSpotsY,rad - 2,rad - 2)
+      ellipse(j - offsetX / 3,i - offsetY / 3 + distanceBetweenSpotsY,rad - 2,(rad - 2) * radYTweak)
       fill (iceMidColor)
-      ellipse(j - offsetX / 2,i - offsetY / 2 + distanceBetweenSpotsY,rad - 1,rad - 1)
+      ellipse(j - offsetX / 2,i - offsetY / 2 + distanceBetweenSpotsY,rad - 1,(rad - 1) * radYTweak)
       fill (iceDarkColor)
-      ellipse(j - offsetX,i - offsetY + distanceBetweenSpotsY,rad,rad)
+      ellipse(j - offsetX,i - offsetY + distanceBetweenSpotsY,rad,rad * radYTweak)
     }
     if (i - distanceBetweenSpotsY < 0) {
       for (j = width; j > 0; j = j - distanceBetweenSpotsX) {
         distanceBetweenSpotsX = 20 + random() * 60;
         distanceBetweenSpotsY = random() * 10 - 10
+        radYTweak = 1 + random();
         fill (iceLightColor)
-        ellipse(j - offsetX / 3,i - offsetY / 3 + distanceBetweenSpotsY,rad - 2,rad - 2)
+        ellipse(j - offsetX / 3,i - offsetY / 3 + distanceBetweenSpotsY,rad - 2,(rad - 2) * radYTweak)
         fill (iceMidColor)
-        ellipse(j - offsetX / 2,i - offsetY / 2 + distanceBetweenSpotsY,rad - 1,rad - 1)
+        ellipse(j - offsetX / 2,i - offsetY / 2 + distanceBetweenSpotsY,rad - 1,(rad - 1) * radYTweak)
         fill (iceDarkColor)
-        ellipse(j - offsetX,i - offsetY + distanceBetweenSpotsY,rad,rad)
+        ellipse(j - offsetX,i - offsetY + distanceBetweenSpotsY,rad,rad * radYTweak)
       }
     }
     distanceBetweenSpotsY = 10 + random() * 60;
